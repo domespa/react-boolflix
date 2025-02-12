bb01b13c4347545b48ac0689e5adc552c042a46c
domespa <dumiii1988@gmail.com>
2025-02-12 12:16:59 +0100

    add usestate for the searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,19 @@
+// 1 PER TENERE TRACCIA DELL'INPUT IMPORTO USESTATE
+import { useState } from "react";
+
 export default function SearchBar() {
+  const [search, setSearch] = useState(""); // 1
+
   return (
     <form>
-      <input type="search" name="" id="" placeholder="Cerca" />
+      <input
+        type="search"
+        name=""
+        id=""
+        placeholder="Cerca"
+        value={search} // 1
+        onChange={(e) => setSearch(e.target.value)} // 1
+      />
       <button type="submit">Cerca</button>
     </form>
   );
