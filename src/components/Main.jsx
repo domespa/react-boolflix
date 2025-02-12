55bb453cde525f3b986b67dd6601114245d73aa7
domespa <dumiii1988@gmail.com>
2025-02-12 18:28:25 +0100

    add function for the flags

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,22 @@ import { useDataContext } from "../contexts/DataContext";
 
 export default function Main() {
   const { movies } = useDataContext();
+
+  // CREO UNA FUNZIONE PER IMPLEMENTARE LE BANDIERE
+  function flags(lang) {
+    const nationflags = {
+      en: "EN",
+      it: "IT",
+      es: "ES",
+      fr: "FR",
+      de: "DE",
+    };
+    const nationflag = nationflags[lang];
+    return (
+      nationflag &&
+      `https://purecatamphetamine.github.io/country-flag-icons/3x2/${nationflag}.svg`
+    );
+  }
   return (
     <main>
       <h2>Lista Film</h2>
@@ -13,6 +29,11 @@ export default function Main() {
             Titolo Originale: <strong>{movie.original_title}</strong>
             <br />
             Lingua: <strong>{movie.original_language}</strong>
+            <img
+              className="flag-cont"
+              src={flags(movie.original_language)}
+              alt=""
+            ></img>
             <br />
             Voto: <strong>{movie.vote_average}</strong>
             <br />
