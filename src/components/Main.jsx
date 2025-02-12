839faa8cead733c73b413a624b2049cc23fc517a
domespa <dumiii1988@gmail.com>
2025-02-12 17:47:05 +0100

    milestone 1

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-// IMPORTO IL CONTESTO PER ADARE A PRENDERE ITERARE SU MOVIES
+// IMPORTO IL CONTESTO PER ADARE A ITERARE SU MOVIES
 import { useDataContext } from "../contexts/DataContext";
 
 export default function Main() {
@@ -8,7 +8,15 @@ export default function Main() {
       <h2>Lista Film</h2>
       <ul>
         {movies.map((movie) => (
-          <li key={movie.id}>{movie.title}</li>
+          <li key={movie.id}>
+            Titolo: <strong>{movie.title}</strong> <br />
+            Titolo Originale: <strong>{movie.original_title}</strong>
+            <br />
+            Lingua: <strong>{movie.original_language}</strong>
+            <br />
+            Voto: <strong>{movie.vote_average}</strong>
+            <br />
+          </li>
         ))}
       </ul>
     </main>
